4c0c36ec641b9c2f28a67fcb5bcdc7023c0f0cf8
GabrielGDL <Gabrieldliberatti@hotmail.com>
2022-05-23 23:39:53 -0300

    commit

---
+++ REACT_JS_ELGELADON/cardapio-el-geladon1/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import "./Home.css"
+import PaletaLista from "./PaletaLista.css"
+
+
+
+
+function Home(){
+    return (
+        <div className="Home"
+    style={{margin : "15px", padding: "15px"}}> 
+    <PaletaLista/>
+    </div>
+    )
+}
+
+export default Home
---
+++ REACT_JS_ELGELADON/cardapio-el-geladon1/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './components/Home';
+import PaletaLista from './components/PaletaLista.jsx';
+import './index.css';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Home/>
+    <PaletaLista/>
+  </React.StrictMode>
+);
+
+
